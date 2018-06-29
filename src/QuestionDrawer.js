c6494d0b0e7455fd37dc7885fdb5502842eca583
gitmarkhubmunar <JeffMunar@jm.lan>
2018-06-28 22:42:44 -0700

    updates to questionnaire

--- src/QuestionDrawer.js
+++ src/QuestionDrawer.js
@@ -22,8 +22,8 @@ class QuestionDrawer extends React.Component {
                             <div className="topic-unanswered">sex</div>
                         </div>
                         <div className="input-area">
-                            <div>male pe3n</div>
-                            <div>female</div>
+                            <div className="button-u">male</div>
+                            <div className="button-u">female</div>
                         </div>
                     </div>
                     <div className="flex-row">
@@ -38,22 +38,43 @@ class QuestionDrawer extends React.Component {
                         </div>
                     </div>
                     <div className="flex-row">
-                        <div>
-                            <object className="variable-icon-unanswered" data="/assets/stage.svg" type="image/svg+xml" />
+                        <div className="icon-label">
+                            <div>
+                                <object className="variable-icon-unanswered" data="/assets/stage.svg" type="image/svg+xml" />
+                            </div>
+                            <div className="topic-unanswered">stage</div>
                         </div>
-                        <div className="topic-unanswered">stage</div>
+                        <div className="button-stage">
+                            <div>1</div>
+                            <div>2</div>
+                            <div>3</div>
+                            <div>4</div>
+                        </div>
                     </div> 
                     <div className="flex-row">
-                        <div>
-                            <object className="variable-icon-unanswered" data="/assets/grade.svg" type="image/svg+xml" />
-                        </div>
-                        <div className="topic-unanswered">grade</div>
-                    </div>            
+                        <div className="icon-label">
+                                <div>
+                                    <object className="variable-icon-unanswered" data="/assets/grade.svg" type="image/svg+xml" />
+                                </div>
+                                <div className="topic-unanswered">grade</div>
+                            </div>
+                            <div className="input-area">
+                                <div className="button-u">well</div>
+                                <div className="button-u">moderately</div>
+                                <div className="button-u">moderately</div>
+                            </div> 
+                        </div>               
                     <div className="flex-row">
-                        <div>
-                            <object className="variable-icon-unanswered" data="/assets/diagnosed.svg" type="image/svg+xml" />
-                        </div>
-                        <div className="topic-unanswered">diagnosed</div>
+                            <div className="icon-label">
+                                <div>
+                                    <object className="variable-icon-unanswered" data="/assets/diagnosed.svg" type="image/svg+xml" />
+                                </div>
+                                <div className="topic-unanswered">diagnosed</div>
+                            </div>
+                            <div className="input-area">
+                                <div>age control goes here</div>
+
+                            </div>
                     </div>
                 </div>
             </div>
