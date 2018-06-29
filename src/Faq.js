091c854e4b4cad2494056cd638f7bd8ff8297aa5
Adam Baker <ab@merges.net>
2018-06-29 00:11:19 -0700

    Stylize questions

--- src/Faq.js
+++ src/Faq.js
@@ -2,9 +2,26 @@ import React from 'react'
 
 class Faq extends React.Component {
     render () {
-        return <div className="faq">
-          This is FAQ
-        </div>
+        return (
+          <div className="faq">
+            <h3>Questions to ask your doctor</h3>
+            <div className="faq-question flex-row nowrap">
+              <div>1</div>
+              <div>Can you verify my stage and grade?</div>
+              <div>+</div>
+            </div>
+            <div className="faq-question flex-row nowrap">
+              <div>2</div>
+              <div>What is your estimate of my survival rate?</div>
+              <div>+</div>
+            </div>
+            <div className="faq-question flex-row nowrap">
+              <div>3</div>
+              <div>Who be what?</div>
+              <div>+</div>
+            </div>
+          </div>
+        )
     }
 }
 
