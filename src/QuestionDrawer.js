e8082551b79db097571a80a85878f344ab8141df
Adam Baker <ab@merges.net>
2018-06-28 23:38:08 -0700

    Tidy up question buttons

--- src/QuestionDrawer.js
+++ src/QuestionDrawer.js
@@ -14,7 +14,7 @@ class QuestionDrawer extends React.Component {
         return (
             <div className="variables">
                 <div onClick={() => switchQuestion('sex')} className="variable-switch-unanswered">
-                    <div className="flex-row">
+                    <div className="flex-row nowrap">
                         <div className="icon-label">
                             <div>
                                 <object className="variable-icon-unanswered" data="/assets/sex.svg" type="image/svg+xml" />
@@ -26,7 +26,7 @@ class QuestionDrawer extends React.Component {
                             <div className="button-u">female</div>
                         </div>
                     </div>
-                    <div className="flex-row">
+                    <div className="flex-row nowrap">
                         <div className="icon-label">
                             <div>
                                 <object className="variable-icon-unanswered" data="/assets/age.svg" type="image/svg+xml" />
@@ -37,44 +37,43 @@ class QuestionDrawer extends React.Component {
                             <div>age control goes here</div>
                         </div>
                     </div>
-                    <div className="flex-row">
+                    <div className="flex-row nowrap">
                         <div className="icon-label">
                             <div>
                                 <object className="variable-icon-unanswered" data="/assets/stage.svg" type="image/svg+xml" />
                             </div>
                             <div className="topic-unanswered">stage</div>
                         </div>
-                        <div className="button-stage">
-                            <div>1</div>
-                            <div>2</div>
-                            <div>3</div>
-                            <div>4</div>
+                        <div className="input-area">
+                            <div className="button-u">1</div>
+                            <div className="button-u">2</div>
+                            <div className="button-u">3</div>
+                            <div className="button-u">4</div>
                         </div>
                     </div> 
-                    <div className="flex-row">
+                    <div className="flex-row nowrap">
                         <div className="icon-label">
-                                <div>
-                                    <object className="variable-icon-unanswered" data="/assets/grade.svg" type="image/svg+xml" />
-                                </div>
-                                <div className="topic-unanswered">grade</div>
+                            <div>
+                                <object className="variable-icon-unanswered" data="/assets/grade.svg" type="image/svg+xml" />
                             </div>
-                            <div className="input-area">
-                                <div className="button-u">well</div>
-                                <div className="button-u">moderately</div>
-                                <div className="button-u">moderately</div>
-                            </div> 
-                        </div>               
-                    <div className="flex-row">
-                            <div className="icon-label">
-                                <div>
-                                    <object className="variable-icon-unanswered" data="/assets/diagnosed.svg" type="image/svg+xml" />
-                                </div>
-                                <div className="topic-unanswered">diagnosed</div>
+                            <div className="topic-unanswered">grade</div>
+                        </div>
+                        <div className="input-area">
+                            <div className="button-u">well</div>
+                            <div className="button-u">moderately</div>
+                            <div className="button-u">moderately</div>
+                        </div> 
+                    </div>               
+                    <div className="flex-row nowrap">
+                        <div className="icon-label">
+                            <div>
+                                <object className="variable-icon-unanswered" data="/assets/diagnosed.svg" type="image/svg+xml" />
                             </div>
-                            <div className="input-area">
-                                <div>age control goes here</div>
-
-                            </div>
+                            <div className="topic-unanswered">diagnosed</div>
+                        </div>
+                        <div className="input-area">
+                            <div>age control goes here</div>
+                        </div>
                     </div>
                 </div>
             </div>
