d7f4f4ec8a9fef02962dadb3dedbfcfeda10e4bc
Adam Baker <merges@users.noreply.github.com>
2018-09-17 16:13:05 -0700

    Tidy up FAQ html

--- src/Faq.js
+++ src/Faq.js
@@ -2,28 +2,28 @@ import React from 'react'
 
 import ExpandedContent from './ExpandedContent'
 import FaqQuestion from './FaqQuestion'
-import ShareCard from './ShareCard'
 import FaqTwo from './FaqTwo'
+import ShareCard from './ShareCard'
 
 class Faq extends React.Component {
     render () {
-      return (
-        <div className="faq">
-          <div></div>
-            <div className="topic-cell">
-                <h3>Questions to ask your doctor</h3>
-                <div className="faq-question flex-row nowrap border-bottom-white">
-                    <div className="number-bullet">1</div>
-                    <div className="suggestions-flat">
-                    Can you verify my stage and grade?
+        return (
+            <div className="faq">
+                <div className="topic-cell">
+                    <h3>Questions to ask your doctor</h3>
+                    <div className="faq-question flex-row nowrap border-bottom-white">
+                        <div className="number-bullet">1</div>
+                        <div className="suggestions-flat">
+                            Can you verify my stage and grade?
+                        </div>
+                        <div className="expand-icon"></div>
                     </div>
-                    <div className="expand-icon"></div>
                 </div>
 
                 <div className="faq-question flex-row nowrap border-bottom-white">
                     <div className="number-bullet">2</div>
                     <div className="suggestions-flat">
-                    What is your estimate of my survival rate for 1, 3, and 5 years?
+                        What is your estimate of my survival rate for 1, 3, and 5 years?
                     </div>
                     <div className="expand-icon"></div>
                 </div>
@@ -31,23 +31,13 @@ class Faq extends React.Component {
                 <div className="faq-question flex-row nowrap border-bottom-white ">
                     <div className="number-bullet">3</div>
                     <div className="suggestions-flat">
-                    Is there anything I can do to improve my prognosis (outlook)?
+                        Is there anything I can do to improve my prognosis (outlook)?
                     </div>
                     <div className="expand-icon"></div>
                 </div>
                 <FaqTwo />
             </div>
-            <div>
-
-            </div>
-
-           
-
-
-        </div>
-
-
-)
+        )
     }
 }
 
