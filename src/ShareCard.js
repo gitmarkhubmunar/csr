a4ca5695ca155e3d8b03955de26d1c40d3c0b330
gitmarkhubmunar <JeffMunar@jm.local>
2018-09-17 16:30:19 -0700

    modal pop update
    blackout and card has been added

--- src/ShareCard.js
+++ src/ShareCard.js
@@ -13,30 +13,31 @@ class ShareCard extends React.Component {
         const { copied } = this.state
 
         return ( 
-            <div className="share-card center-all">
-                <div className="share-card-close-button" onClick={() => onClose()}>
-                    <Icon icon="ios-close" fontSize="4em" style={{ fill: '#999' }} />
-                </div>
-                <div className="content-container share-card-content-container relative">
-                    <h2>Share this information with others.</h2>
-                    <p>Click copy and paste in an email, SMS, etc.</p>
-                    <div className="flex-row">
-                        <div className="share-link-box flex-auto center-all">{shareableUrl}</div>
-                        <CopyToClipboard
-                            text={shareableUrl}
-                            onCopy={() => this.setState({ copied: true })}>
-                            <div className="share-link-copy-button center-all">COPY</div>
-                        </CopyToClipboard>
+            <div className="blackout">
+                <div className="share-card center-all">
+                    <div className="share-card-close-button" onClick={() => onClose()}>
+                        <Icon icon="ios-close" fontSize="4em" style={{ fill: '#999' }} />
                     </div>
-                    <div className="relative">
-                        {copied &&
-                            <div className="share-card-copied-message">
-                                Link copied to clipboard
-                            </div>
-                        }
+                    <div className="content-container share-card-content-container relative">
+                        <h2>Share this information with others.</h2>
+                        <p>Click copy and paste in an email, SMS, etc.</p>
+                        <div className="flex-row">
+                            <div className="share-link-box flex-auto center-all">{shareableUrl}</div>
+                            <CopyToClipboard
+                                text={shareableUrl}
+                                onCopy={() => this.setState({ copied: true })}>
+                                <div className="share-link-copy-button center-all">COPY</div>
+                            </CopyToClipboard>
+                        </div>
+                        <div className="relative">
+                            {copied &&
+                                <div className="share-card-copied-message">
+                                    Link copied to clipboard
+                                </div>
+                            }
+                        </div>
                     </div>
                 </div>
-                
             </div>
         )
     }
