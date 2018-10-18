3d41f0529a307cf5eb9f64053156fb05f18623f0
Adam Baker <ab@merges.net>
2018-10-17 17:35:38 -0700

    Rename standard variables key in cancer data; conditionally show questions
    Rename standard variables key in cancer data; conditionally show
    questions

--- src/QuestionDrawer.js
+++ src/QuestionDrawer.js
@@ -15,39 +15,61 @@ class QuestionDrawer extends React.Component {
     render () {
         const { age, changeValue, diagnosed, grade, selectedCancerType, sex, stage } = this.props
         const selectedCancer = _.find(CancerTypes, { id: selectedCancerType })
-        const color = selectedCancer.colors[0]
+        const color = _.get(selectedCancer, 'colors.[0]', 'black')
+
+        // Customize questions based on each cancer type’s 'showStandardVariables' key;
+        // if unavailable, show the default set.
+        const defaultStandardVariables = {
+            age: true,
+            diagnosis: true,
+            grade: true,
+            sex: true,
+            stage: true,
+        }
+        const shouldShow = _.get(selectedCancer, 'showStandardVariables', defaultStandardVariables)
+
         return (
             <div className="question-drawer variables">
-                <QuestionSex
-                    changeValue={changeValue}
-                    className="variable-row"
-                    color={color}
-                    sex={sex}
-                />
-                <QuestionAge
-                    age={age}
-                    changeValue={changeValue}
-                    className="variable-row"
-                    color={color}
-                />
-                <QuestionStage
-                    changeValue={changeValue}
-                    className="variable-row"
-                    color={color}
-                    stage={stage}
-                />
-                <QuestionGrade
-                    changeValue={changeValue}
-                    className="variable-row"
-                    color={color}
-                    grade={grade}
-                />
-                <QuestionDiagnosed
-                    changeValue={changeValue}
-                    className="variable-row"
-                    color={color}
-                    diagnosed={diagnosed}
-                />
+                {shouldShow['sex'] &&
+                    <QuestionSex
+                        changeValue={changeValue}
+                        className="variable-row"
+                        color={color}
+                        sex={sex}
+                    />
+                }
+                {shouldShow['age'] &&
+                    <QuestionAge
+                        age={age}
+                        changeValue={changeValue}
+                        className="variable-row"
+                        color={color}
+                    />
+                }
+                {shouldShow['stage'] &&
+                    <QuestionStage
+                        changeValue={changeValue}
+                        className="variable-row"
+                        color={color}
+                        stage={stage}
+                    />
+                }
+                {shouldShow['grade'] &&
+                    <QuestionGrade
+                        changeValue={changeValue}
+                        className="variable-row"
+                        color={color}
+                        grade={grade}
+                    />
+                }
+                {shouldShow['diagnosed'] &&
+                    <QuestionDiagnosed
+                        changeValue={changeValue}
+                        className="variable-row"
+                        color={color}
+                        diagnosed={diagnosed}
+                    />
+                }
             </div>
         )
     }
