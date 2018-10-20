933db8d84d3e3710766c10c1c686cc4a5ed44d9c
gitmarkhubmunar <jeff@thevisual.work>
2018-10-19 18:43:54 -0700

    NoCalc Doctor mode
    styles from noCalc patient is applied to the Doc version.
    
    (may not need this panel)

--- src/NoCalcDoctor.js
+++ src/NoCalcDoctor.js
@@ -6,14 +6,14 @@ class NoCalcDoctor extends React.Component {
 		return (
              <div className="patient-alt-container-grid">
                 <div></div>
-                <div className="nested flex-center">
-                     <div className="alt-header">Stay Tuned.</div>
-                     <div className="alt-copy flex-center">We currently do not have a calculator or tables for {this.props.selectedCancer.name}.</div>
-                     <div className="alt-copy flex-center">Please check back in the future or email info@cancersurvivalrates.org if you would like to inquire further or have questions</div>
+                <div className="alt-header marginr">
+                     <div className="alt-headline">Stay Tuned.</div>
+                     <div className="alt-sub flex-center">We currently do not have a calculator or tables for {this.props.selectedCancer.name}.</div>
+                     <div className="alt-sub flex-center">Please check back in the future or email info@cancersurvivalrates.org if you would like to inquire further or have questions</div>
                      <img
                         className="alt-image"
                         src="../assets/checkback.svg"
-                        style={{ width: '80px' }}
+                        style={{ width: '40px' }}
                     />
                 </div>
                 <div></div>
