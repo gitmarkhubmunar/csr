2c9616f771faa02ea8c6cb69de540416b30146cb
gitmarkhubmunar <mr.munar@gmail.com>
2018-10-24 12:15:33 -0700

    minor updates (#45)
    Reset the icon styling
    
    Minor improvements are made to the Question panel

--- src/QuestionDiagnosed.js
+++ src/QuestionDiagnosed.js
@@ -2,31 +2,32 @@ import React from 'react'
 import ReactSlider from 'react-slider'
 
 const diagnosedLabels = {
-    '0': 'Within the past month',
-    '1': '1 month ago',
-    '2': '2 months ago',
-    '3': '3 months ago',
-    '4': '4 months ago',
-    '5': '5 months ago',
-    '6': '6 months ago',
-    '7': '7 months ago',
-    '8': '8 months ago',
-    '9': '9 months ago',
-    '10':  '10 months ago',
-    '11':  '11 months ago',
-    '12':  '1 year ago?',
-    '13':  '1 year 1 month ago',
-    '14':  '1 year 2 months ago',
-    '15':  '1 year 3 months ago',
-    '16':  '1 year 4 months ago',
-    '17':  '1 year 5 months ago',
-    '18':  '1 year 6 months ago',
-    '19':  '1 year 7 months ago',
-    '20':  '1 year 8 months ago',
-    '21':  '1 year 9 months ago',
-    '22':  '1 year 10 months ago',
-    '23':  '1 year 11 months ago',
-    '24':  '2 years ago'
+    '0': 'null',
+    '1': 'Within past month',
+    '2': '1 month ago',
+    '3': '2 months ago',
+    '4': '3 months ago',
+    '5': '4 months ago',
+    '6': '5 months ago',
+    '7': '6 months ago',
+    '8': '7 months ago',
+    '9': '8 months ago',
+    '10':  '9 months ago',
+    '11':  '10 months ago',
+    '12':  '11 months ago',
+    '13':  '1 year ago',
+    '14':  '1 year 1 month ago',
+    '15':  '1 year 2 months ago',
+    '16':  '1 year 3 months ago',
+    '17':  '1 year 4 months ago',
+    '18':  '1 year 5 months ago',
+    '19':  '1 year 6 months ago',
+    '20':  '1 year 7 months ago',
+    '21':  '1 year 8 months ago',
+    '22':  '1 year 9 months ago',
+    '23':  '1 year 10 months ago',
+    '24':  '1 year 11 months ago',
+    '25':  '2 years ago',
 }
 
 class QuestionDiagnosed extends React.Component {
@@ -63,7 +64,7 @@ class QuestionDiagnosed extends React.Component {
                         <ReactSlider
                             className="horizontal-slider bar"
                             value={nextDiagnosed}
-                            max={24}
+                            max={25}
                             onAfterChange={(value) => changeValue('diagnosed', value)}
                             onChange={(value) => this.setState({ nextDiagnosed: value })}
                         >
@@ -82,7 +83,7 @@ class QuestionDiagnosed extends React.Component {
                         </ReactSlider>
                         <div className="slider-ticks">
                             <div className="slider-tick-button" style={{ left: 0 }} onClick={() => changeValue('diagnosed', 0)}>Within past month</div>
-                            <div className="slider-tick-button" style={{ right: 0 }} onClick={() => changeValue('diagnosed', 24)}>2 years ago</div>
+                            <div className="slider-tick-button" style={{ right: 0 }} onClick={() => changeValue('diagnosed', 25)}>2 years ago</div>
                         </div>
                     </div>
                 </div>
