54532744db91806622c82bdbe606b10871a80775
gitmarkhubmunar <jeff@thevisual.work>
2018-10-29 21:03:25 -0700

    homepage dropdown refinement
    the hp drop is more refined and responsive
    
    hacked a bit in iOS to have it sit well
    within the new headline layout

--- src/HomePage.js
+++ src/HomePage.js
@@ -60,19 +60,24 @@ class HomePage extends React.Component {
         return (
             <div className="home-page-container">
                 <div className="home-page content-container">
-                    <h2>I’m looking for answers on cancer survival rates for</h2> 
-                     <div className="dropdown-home mobile-dropdown-container-home mobile-dropdown-home">
-                        <Dropdown
-                            className="title-dropdown text-center cancer-title"
-                            list={CancerTypes.map(type => type.name)}
-                            onSelect={name => {
-                                const id = _.find(CancerTypes, { name: name }).id
-                                this.setState({ selectedCancerType: id })
-                            }}
-                            selectedItem={selectedCancer.name}
-                        />
-                        <img className="caret home-page-caret" src="../assets/dropdown.svg" />
-                    </div>
+                <div className="headline-container"> 
+                    <h2 className="headline">I’m looking for answers on survival rates for</h2> 
+                         <div className="dropdown-home mobile-dropdown-container-home mobile-dropdown-home">
+                        
+                                <Dropdown
+                                    className="title-dropdown text-center cancer-title"
+                                    list={CancerTypes.map(type => type.name)}
+                                    onSelect={name => {
+                                        const id = _.find(CancerTypes, { name: name }).id
+                                        this.setState({ selectedCancerType: id })
+                                    }}
+                                    selectedItem={selectedCancer.name}
+                                />
+                                <img className="caret home-page-caret" src="../assets/dropdown.svg" />
+                                </div>
+                            </div>
+                            <h2 className="cancer-word">cancer.</h2>
+                    
                         <div className="toggle-copy-container">
                             <h3 className="toggle-copy">Healthcare Professional?</h3>
                             <div className="toggle-container">
