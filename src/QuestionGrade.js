772726a2cae93f037f68111a5879b590aa8932e7
Adam Baker <ab@merges.net>
2018-11-19 12:15:29 -0800

    Various fixes

--- src/QuestionGrade.js
+++ src/QuestionGrade.js
@@ -1,11 +1,13 @@
+import CheckMobile from 'is-mobile'
 import React from 'react'
-import {Tooltip,} from 'react-tippy';
+import { Tooltip } from 'react-tippy'
 
 import ButtonSurvey from './ButtonSurvey'
 
 class QuestionGrade extends React.Component {
     render () {
         const { changeValue, className, color, grade } = this.props
+        const isMobile = CheckMobile()
         return (
             <div className={className}>
                 <div className="icon-label">
@@ -15,49 +17,59 @@ class QuestionGrade extends React.Component {
                     <div className={grade === null ? 'variable-name' : 'variable-name answered'}>grade</div>
                 </div>
                 <div className="input-area">
-
-                <div className="tippy-container">
-                    <Tooltip
-                      className="tippy"
-                      title="Well (Grade 1)"
-                      position="bottom"
-                      trigger="mouseenter"
-                      position= "top"
-                      size="big"
-                    > 
-                      <p>
-                            <ButtonSurvey color={color} handleClick={() => changeValue('grade', '1')} className="" name="well" turnedOn={grade === '1'} />
-                      </p>
-                    </Tooltip>
+                    {isMobile ?
+                        <ButtonSurvey color={color} handleClick={() => changeValue('grade', '1')} name="well" turnedOn={grade === '1'} />
+                        :
+                        <div className="tippy-container">
+                            <Tooltip
+                                className="tippy"
+                                title="Well (Grade 1)"
+                                position="bottom"
+                                trigger="mouseenter"
+                                position= "top"
+                                size="big"
+                            > 
+                                <div>
+                                      <ButtonSurvey color={color} handleClick={() => changeValue('grade', '1')} name="well" turnedOn={grade === '1'} />
+                                </div>
+                            </Tooltip>
+                        </div>
+                    }
+                    {isMobile ?
+                        <ButtonSurvey color={color} handleClick={() => changeValue('grade', '2')} name="moderately" turnedOn={grade === '2'} />
+                        :
+                        <div className="tippy-container">
+                            <Tooltip
+                                title="Moderately (Grade 2)"
+                                position="bottom"
+                                trigger="mouseenter"
+                                position= "top"
+                                size="big"
+                            >
+                                <div>
+                                      <ButtonSurvey color={color} handleClick={() => changeValue('grade', '2')} name="moderately" turnedOn={grade === '2'} />
+                                </div>
+                            </Tooltip>
+                        </div>
+                    }
+                    {isMobile ?
+                        <ButtonSurvey color={color} handleClick={() => changeValue('grade', '3')} name="poorly" turnedOn={grade === '3'} />
+                        :
+                        <div className="tippy-container">
+                            <Tooltip
+                                title="Poorly & Undifferentiated (Grade 3/4)"
+                                position="bottom"
+                                trigger="mouseenter"
+                                position= "top"
+                                size="big"
+                            > 
+                                <div>
+                                      <ButtonSurvey color={color} handleClick={() => changeValue('grade', '3')} name="poorly" turnedOn={grade === '3'} />
+                                </div>
+                            </Tooltip>
+                        </div>
+                    }
                 </div>
-                <div className="tippy-container">
-                    <Tooltip
-                      title="Moderately (Grade 2)"
-                      position="bottom"
-                      trigger="mouseenter"
-                      position= "top"
-                      size="big"
-                    > 
-                      <p>
-                            <ButtonSurvey color={color} handleClick={() => changeValue('grade', '2')} className="" name="moderately" turnedOn={grade === '2'} />
-                      </p>
-                    </Tooltip>
-                </div>
-                <div className="tippy-container">
-                    <Tooltip
-                      title="Poorly & Undifferentiated (Grade 3/4)"
-                      position="bottom"
-                      trigger="mouseenter"
-                      position= "top"
-                      size="big"
-                    > 
-                      <p>
-                            <ButtonSurvey color={color} handleClick={() => changeValue('grade', '3')} className="" name="poorly" turnedOn={grade === '3'} />
-                      </p>
-                    </Tooltip>
-                </div>
-
-                </div> 
             </div>
         )
     }
